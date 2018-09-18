a7a01e41b9afc7451111fd6059286d13775c868c
Rafael <>
2018-09-18 12:08:07 -0300

    Merge origin/master
    Conflicts:
    	busca-cega-jogo/nbproject/build-impl.xml
    	busca-cega-jogo/nbproject/genfiles.properties
    	busca-cega-jogo/nbproject/private/private.properties
    	busca-cega-jogo/src/br/com/ia/view/MainViewJogo.java

--- busca-cega-jogo/src/br/com/ia/view/MainViewJogo.java
+++ busca-cega-jogo/src/br/com/ia/view/MainViewJogo.java
@@ -1,4 +1,4 @@
-/*Commit TEST
+/*Commit TEST2
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
